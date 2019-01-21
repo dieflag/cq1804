73a8f93f2d61f2e79f8ea483458cc8c7680ae757
dieflag <736057588@qq.com>
2019-01-21 11:42:00 +0800

    @upDate 2019-01-21

--- src/main/java/com/cqkj/entity/CW17Do.java
+++ src/main/java/com/cqkj/entity/CW17Do.java
@@ -25,7 +25,7 @@ public class CW17Do extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(cw.HelloWorld());
+		response.getWriter().append("Served at: ").append(cw.HelloWorld() +"!");
 	}
 
 	/**
